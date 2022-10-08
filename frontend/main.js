ace02bb0e02b3535222b96e8b35e82a23c7c6fce
Your Name <augustbotezatu@t-online.de>
2022-10-08 21:28:23 +0200

    frontent weiteres design

--- frontend/main.js
+++ frontend/main.js
@@ -1,10 +1,10 @@
-
-
+let recentSearch = "";
+console.log("start");
 function test() {
     //const h2 = document.createElement('h2'); console.log(h2)
     //h2.innerHTML = "ASDajfuegvis"
     //document.getElementById("js-container").appendChild(h2);
-    const names = ['a', 'b', 'c'];
+    const names = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
     const ul = document.querySelector("#js-container > ul");
     for (let i = 0; i < names.length; i ++) {
@@ -14,4 +14,22 @@ function test() {
         ul.appendChild(li);
     }
 }
-document.addEventListener("DOMContentLoaded", test)
+
+function requestSearch(searchTerm) {
+    console.log(searchTerm);
+}
+
+function init() {
+    test();
+    const searchButton = document.getElementById('search-button');
+    const searchInput = document.getElementById('search-input');
+    searchButton.addEventListener('click', () => {
+
+        const inputValue = searchInput.value;
+        if (inputValue !== recentSearch) {
+            requestSearch(inputValue);
+        }
+    });
+}
+
+document.addEventListener("DOMContentLoaded", init);
