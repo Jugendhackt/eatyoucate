65d3e493648eb981a0f00b3351bb258b8d824128
LeSnake04 <dev.lesnake@posteo.de>
2022-10-08 12:10:03 +0200

    added list_products

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,30 @@
+use rocket::serde::{json::Json, Serialize};
 use rocket::{get, launch, routes};
+const PRODUCTS: [&str; 3] = ["banana", "apple", "rice"];
+
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+pub struct ProductList {
+    data: Vec<&'static str>,
+}
+
+#[get("/products?<term>")]
+fn list_products(term: Option<&str>) -> Json<ProductList> {
+    let mut out: Vec<&str> = vec![];
+    match term {
+        Some(t) => {
+            for product in PRODUCTS {
+                if product.contains(t) {
+                    out.push(product)
+                }
+            }
+        }
+        None => {
+            out = PRODUCTS.to_vec();
+        }
+    }
+    Json(ProductList { data: out })
+}
 
 #[get("/")]
 fn hello() -> &'static str {
@@ -7,5 +33,5 @@ fn hello() -> &'static str {
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![hello])
+    rocket::build().mount("/", routes![hello, list_products])
 }
